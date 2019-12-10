99d87d41d781d163cf7cb03d03a9ca4d65105ece
Andres Quiroga <contacto@andresquiroga.com>
2019-12-10 06:19:24 -0500

    Beeter code

--- clear_jobs.py
+++ clear_jobs.py
@@ -1,27 +1,12 @@
-import sys
+import os
+
+from datetime import datetime
 
 from kubernetes import client
 from kubernetes import config
 from kubernetes.client.rest import ApiException
 
-arguments = sys.argv
-
-# Delete first argument (script name)
-del arguments[0]
-
-valid_arguments = [
-    'namespace',
-    'cronjob-name',
-]
-
-arguments_dict = {}
-
-for argument in arguments:
-    argument_name, argument_value = argument.split('=')
-    arguments_dict[argument_name] = argument_value
-
-namespace = arguments_dict['namespace']
-cronjob_name = arguments_dict['cronjob-name']
+namespace = os.environ.get('NAMESPACE')
 
 config.load_incluster_config()
 k8s_batch_client = client.BatchV1Api()
@@ -33,10 +18,7 @@ try:
         status = job.status
         job_name = job.metadata.labels['job-name']
 
-        if (
-            status.failed or
-            status.succeeded and cronjob_name in job_name
-        ):
+        if status.failed:
             k8s_batch_client.delete_namespaced_job(
                 name=job_name,
                 namespace=namespace,
@@ -44,6 +26,20 @@ try:
                     propagation_policy='Foreground',
                 )
             )
+
+        if status.succeeded:
+            now = datetime.now()
+            completion_time = status.completion_time
+            secconds_diff = (now - completion_time.replace(tzinfo=None)).total_seconds()
+
+            if secconds_diff >= 60:
+                k8s_batch_client.delete_namespaced_job(
+                    name=job_name,
+                    namespace=namespace,
+                    body=client.V1DeleteOptions(
+                        propagation_policy='Foreground',
+                    )
+                )
 
 except ApiException as e:
     print("Exception when calling BatchV1Api->list_namespaced_job: %s\n" % e)
